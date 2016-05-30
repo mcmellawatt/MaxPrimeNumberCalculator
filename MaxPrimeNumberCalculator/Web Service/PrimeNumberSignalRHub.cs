9c547146f929a2b536b57c9153aa16042b661ef8
mcmellawatt <mcmellawat@gmail.com>
2016-05-30 13:20:00 -0700

    Adding actual timer and test

--- MaxPrimeNumberCalculator/Web Service/PrimeNumberSignalRHub.cs
+++ MaxPrimeNumberCalculator/Web Service/PrimeNumberSignalRHub.cs
@@ -1,17 +1,45 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System.Timers;
+using Microsoft.AspNet.SignalR;
 
 namespace MaxPrimeNumberCalculator.Web_Service
 {
     public class PrimeNumberSignalRHub : Hub
     {
+        private int _timeToCalculate;
+        private bool _maxTimeReached = false;
+
+        public PrimeNumberSignalRHub(int timeToCalculate = 60)
+        {
+            _timeToCalculate = timeToCalculate;
+        }
+
         public void StartPrimeCalculation()
         {
             UpdateTimer();
         }
 
+        /// <summary>
+        /// Updates the timer in the SignalR clients
+        /// </summary>
         private void UpdateTimer()
         {
-            Clients.Caller.updateTimer(60);
+            //Create and start the timer to update the client
+            //This will run on its own thread
+            var timer = new Timer(1000);
+            var secondsElapsed = 0;
+
+            timer.Elapsed += (sender, args) =>
+            {
+                if (++secondsElapsed == _timeToCalculate)
+                {
+                    _maxTimeReached = true;
+                    timer.Stop();
+                }
+
+                Clients.Caller.updateTimer(secondsElapsed.ToString());
+            };
+
+            timer.Start();
         }
     }
 }
---
+++ MaxPrimeNumberCalculator_Tests/PrimeNumberSignalRHub_Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MaxPrimeNumberCalculator.Web_Service;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MaxPrimeNumberCalculator_Tests
+{
+    [TestClass]
+    public class PrimeNumberSignalRHub_Tests
+    {
+        [TestMethod]
+        public void PrimeNumberSignalRHub_StartPrimeCalculation_TimerUpdated()
+        {
+            int timerUpdatedCount = 0;
+            var timeToCalculate = 5; //Set hub to calculate for 5 seconds
+            var hub = new PrimeNumberSignalRHub(timeToCalculate);
+
+            var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+            hub.Clients = mockClients.Object;
+            dynamic caller = new ExpandoObject();
+
+            var resetEvent = new AutoResetEvent(false);
+
+            caller.updateTimer = new Action<string>((name) => {
+                ++timerUpdatedCount;
+                if (timerUpdatedCount == timeToCalculate)
+                    resetEvent.Set();
+            });
+
+            mockClients.Setup(m => m.Caller).Returns((ExpandoObject)caller);
+            hub.StartPrimeCalculation();
+            resetEvent.WaitOne(6000);
+
+            Assert.AreEqual(timeToCalculate, timerUpdatedCount);
+        }
+    }
+}
