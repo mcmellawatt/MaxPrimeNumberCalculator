cfb63050ce27b8a06521aaba08b568396e010ef2
mcmellawatt <mcmellawat@gmail.com>
2016-05-30 12:52:24 -0700

    Basic Self Hosted SignalR Communication
    Added required dependencies and code for a simple self hosted SignalR
    application.  Includes one hub server method and one SignalR client
    method.

---
+++ MaxPrimeNumberCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+
+namespace MaxPrimeNumberCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            string url = "http://localhost:8080";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //To host the client code static files
+            app.UseFileServer(new FileServerOptions()
+            {
+                EnableDirectoryBrowsing = true
+            });
+
+            //To map the PrimeNumberSignalRHub
+            app.MapSignalR();
+        }
+    }
+}
---
+++ MaxPrimeNumberCalculator/Web Service/PrimeNumberSignalRHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace MaxPrimeNumberCalculator.Web_Service
+{
+    public class PrimeNumberSignalRHub : Hub
+    {
+        public void StartPrimeCalculation()
+        {
+            UpdateTimer();
+        }
+
+        private void UpdateTimer()
+        {
+            Clients.Caller.updateTimer(60);
+        }
+    }
+}
